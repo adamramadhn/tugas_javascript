02458ad34d4322c6091fc8cdac09145245b36780
adamramadhn <adamramadhan340@gmail.com>
2023-06-13 09:27:30 +0700

    add tugas

---
+++ tugas.js
@@ -0,0 +1,60 @@
+// soal no 1
+
+var x = 10;
+if(x<0){
+    console.log('Tidak bisa input bilangan negatif')
+    return;
+}
+if(x%2==0){
+    var y = Math.sqrt(x);
+    console.log(y)
+}
+else{
+    console.log('Tidak bisa input bilangan ganjil')
+}
+
+// soal no 2
+
+let productBin = {
+    "requestId": null,
+    "data": [
+      {
+        "id": 100000057465,
+        "storageId": 10000008207,
+        "code": "A01-01-01-A",
+        "productId": 110000081009,
+        "productName": "FloBrand-DressBSPink",
+        "productCode": "FBR00040101",
+        "quantity": 76,
+        "createdTime": "2021-12-21T13:54:48Z",
+      },
+      {
+        "id": 100000057466,
+        "storageId": 10000002181,
+        "code": "A01-01-01-B",
+        "productId": 110000081009,
+        "productName": "FloBrand-DressBSPink",
+        "productCode": "FBR00040101",
+        "quantity": 71,
+        "createdTime": "2021-12-21T13:54:48Z",
+      },
+      {
+        "id": 100000065224,
+        "storageId": 10000008884,
+        "code": "Tgt00-A-A-01",
+        "productId": 110000081009,
+        "productName": "FloBrand-DressBSPink",
+        "productCode": "FBR00040101",
+        "quantity": 10,
+        "createdTime": "2022-02-08T10:35:19Z",
+      }
+    ],
+    "message": "success"
+  }
+var count = 0;
+for(var i=0;i<productBin.data.length;i++){
+    if(productBin.data[i].productCode == "FBR00040101"){
+        count= count+productBin.data[i].quantity;
+    }
+}
+console.log(count)
